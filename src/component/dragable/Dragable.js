5d323b449fceb0848c2684b333bff943fa58d53b
shazy89 <erdo.shazy123@gmail.com>
2020-12-08 12:59:47 -0500

    draggable

--- src/component/dragable/Dragable.js
+++ src/component/dragable/Dragable.js
@@ -2,26 +2,58 @@ import React, { useState } from "react";
 import { Switch, Route } from 'react-router-dom';
 import Draggable, {DraggableCore} from 'react-draggable';
 import Button from '@material-ui/core/Button'
+import DragableCard from './DragableCard'
+
+
 
 
 const Dragable = () => {
+
     const [position, setPosition] = useState({ x: 0, y: 0 });
+    
      const [rect, setRect] = useState({ width: 200, height: 200, x: 200, y: 200, rotate: 0 })
 
     const trackPos = (data) => {
-        console.log(data)
+    
         setPosition({ x: data.x, y: data.y });
      };
     
     return (
     <div>
-<Draggable   axis='y'>
+<Draggable   axis='y' >
    <Button>axis=”y”: the component can only be dragged vertically.</Button>
  
 </Draggable>
 <Draggable   axis='x'>
    <Button>axis=”x”: the component can only be dragged vertically.</Button>
 </Draggable>
+<Draggable onDrag={(e, data) => trackPos(data)}>
+   <div className="box">
+       <Button>Here's my position...</Button>
+       <div>
+            x: {position.x.toFixed(0)}, y: {position.y.toFixed(0)}
+       </div>
+   </div>
+</Draggable>
+<Draggable handle="strong" >
+          <div className="box no-cursor">
+            <strong  style={{cursor: 'grabbing'}}><div>Drag here</div></strong>
+            <Button>You must click my handle to drag me</Button>
+          </div>
+</Draggable>
+
+<div style={{height: '500px', width: '800px', position: 'relative', overflow: 'auto', padding: '0', marginLeft: '100px'}}>
+<div style={{height: '1000px', width: '1000px', padding: '10px'}}>
+
+    <DragableCard />
+
+   < DragableCard />
+
+ </div>
+</div>
+
+  
+ 
 </div>
  
     )
---
+++ src/component/dragable/DragableCard.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
+import Draggable, {DraggableCore} from 'react-draggable';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      maxWidth: 400
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+
+const DragableCard = () => {
+
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+    return (
+      <Draggable bounds="parent" >   
+   <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Draggable>
+    )
+}
+export default DragableCard;
