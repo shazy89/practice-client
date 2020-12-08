25dfa5808b155bac2395fe78d0b9c50c005c3746
shazy89 <erdo.shazy123@gmail.com>
2020-12-08 10:16:20 -0500

    Home and Routes components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Home from './component/Home'
 
 
 
 function App() {
   return (
-    <div className="App">
-
+    <div >
+       <Home />
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+export default Routes;
